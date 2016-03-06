afd7ec9db9005dd27089b5b56831908fc4501b77
Nick Pearson <Nick.Pearson@gradsingames.com>
2016-03-06 21:37:05 +0000

    Modified CreateMesh and game scene to generate stars instead of arrows

--- UnityProject/Assets/Scripts/CreateMesh.cs
+++ UnityProject/Assets/Scripts/CreateMesh.cs
@@ -16,36 +16,50 @@ public class CreateMesh : MonoBehaviour
 	
 	private Vector3 [] GetVerts( float size )
 	{
-		Vector3 [] verts = new Vector3[7]; 
+		Vector3 [] verts = new Vector3[8]; 
 		float wide = size * 0.5f;
 		float narrow = size * 0.15f;
 		
-		verts[0] = new Vector3( 0.0f, wide, 0.0f );
-		verts[1] = new Vector3( -wide, 0.0f, 0.0f );
-		verts[2] = new Vector3( wide, 0.0f, 0.0f );
-		verts[3] = new Vector3( -narrow, 0.0f, 0.0f );
-		verts[4] = new Vector3( narrow, 0.0f, 0.0f );
-		verts[5] = new Vector3( -narrow, -wide, 0.0f );
-		verts[6] = new Vector3( narrow, -wide, 0.0f );
+		verts[0] = new Vector3(   0.0f,   wide, 0.0f );
+		verts[1] = new Vector3( narrow, narrow, 0.0f );
+		verts[2] = new Vector3(   wide,   0.0f, 0.0f );
+		verts[3] = new Vector3( narrow,-narrow, 0.0f );
+		verts[4] = new Vector3(   0.0f,  -wide, 0.0f );
+		verts[5] = new Vector3(-narrow,-narrow, 0.0f );
+		verts[6] = new Vector3(  -wide,   0.0f, 0.0f );
+        verts[7] = new Vector3(-narrow, narrow, 0.0f );
 
 		return verts;
 	}
 	
 	private int [] GetTriangles()
 	{
-		int [] starTriangles = new int[9];
-		
-		starTriangles[0] = 0;
-		starTriangles[1] = 2;
-		starTriangles[2] = 1;
-		starTriangles[3] = 3;
-		starTriangles[4] = 4;
-		starTriangles[5] = 5;
-		starTriangles[6] = 4;
-		starTriangles[7] = 6;
-		starTriangles[8] = 5;
+		int [] starTriangles = new int[18]; // 6 triangles
 
-		return starTriangles;
+        starTriangles[0] = 0;
+        starTriangles[1] = 1;
+        starTriangles[2] = 7;
+        
+        starTriangles[3] = 1;
+        starTriangles[4] = 2;
+        starTriangles[5] = 3;
+
+        starTriangles[6] = 3;
+        starTriangles[7] = 4;
+        starTriangles[8] = 5;
+
+        starTriangles[9]  = 5;
+        starTriangles[10] = 6;
+        starTriangles[11] = 7;
+
+        starTriangles[12] = 1;
+        starTriangles[13] = 3;
+        starTriangles[14] = 5;
+
+        starTriangles[15] = 1;
+        starTriangles[16] = 5;
+        starTriangles[17] = 7;
+        return starTriangles;
 	}
 	
 	private Mesh DoCreateMesh()
